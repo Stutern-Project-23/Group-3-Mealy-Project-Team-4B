a6e2b8f91b7d658719da28d61b2b6f6d268ddeea
olutola olajide michael <42746141+jidegithub@users.noreply.github.com>
2023-05-26 20:08:01 +0000

    add react router and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import CreateNewPassword from "./components/CreateNewPassword";
 import ForgotPassword from "./components/ForgotPassword";
 import SetDelivery from "./components/SetDelivery";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <SetDelivery/>
-    </div>
-  );
-}
+function App (){
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>index</p>
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword/>
+  },
+  {
+    path: "/delivery",
+    element: <SetDelivery/>
+  },
+  {
+    path: "/create-new-password",
+    element: <CreateNewPassword/>
+  },
+]);
+
+return (
+  <div className="App">
+    <RouterProvider router={router} />
+  </div>
+)}
 
 export default App;
