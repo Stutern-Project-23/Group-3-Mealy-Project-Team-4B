8c7ff36ea10f0fa4c80d80119e9d84c3905b1a38
Adeola Adegbusi <adeadeola234@gmail.com>
2023-05-30 23:25:10 +0100

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import {Routes, Route} from 'react-router-dom';
-import Homepage from './components/Homepage';
-import SignUp from './components/SignUp';
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import SignUp from "./components/SignUp";
+import Verification from "./components/Verification";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <Routes>
-
-      <Route path='/' element={<Homepage/>}>
-
-      </Route>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/verification" element={<Verification />} />
     </Routes>
     // <div className="App">
-    //   {/* <Homepage /> */}
-    //   <SignUp/>
+    //   {<Homepage > }
+    //   <Footer/>
     // </div>
   );
 }
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,27 +1,17 @@
-// import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import React from "react";
-// import SignUp from "../components/SignUp";
 import "../styles/Homepage.css";
 
 const Homepage = () => {
-//   const [showPopup, setShowPopup] = useState(false);
-
-//   const handleGetStartedClick = () => {
-//     setShowPopup(true);
-//   };
-
-//   const handleClosePopup = () => {
-//     setShowPopup(false);
-//   };
-
   return (
     <div>
       <div className="container">
         <h1>MEALY</h1>
-        <button >Get Started</button>
-        {/* <button onClick={handleGetStartedClick}>Get Started</button> */}
+
+        <Link to="/signup">
+          <button>Get Started</button>
+        </Link>
       </div>
-      {/* {showPopup && <SignUp onClose={handleClosePopup} />} */}
     </div>
   );
 };
